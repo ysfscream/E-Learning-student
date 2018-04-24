b75a475fb2fa0f1932a79f4cef09fa8bd4a2b541
ysfscream <894402575@qq.com>
2018-04-24 22:35:32 +0800

    feat: 新增修改密码

--- src/views/Platform/changePassword.vue
+++ src/views/Platform/changePassword.vue
@@ -8,9 +8,15 @@
     <br>
     <v-container grid-list-md text-xs-center>
       <v-layout row wrap>
+        <v-flex xs12 md12>
+          <v-alert type="error" dismissible v-model="error">
+            {{ errorMessage }}
+          </v-alert>
+        </v-flex>
         <v-flex xs12>
           <v-text-field
             label="旧密码"
+            type="password"
             v-model="passwordForm.oldPassword"
             required>
           </v-text-field>
@@ -18,19 +24,21 @@
         <v-flex xs12>
           <v-text-field
             label="新密码"
-            v-model="passwordForm.oldPassword"
+            type="password"
+            v-model="passwordForm.password"
             required>
           </v-text-field>
         </v-flex>
         <v-flex xs12>
           <v-text-field
             label="确认密码"
-            v-model="passwordForm.oldPassword"
+            type="password"
+            v-model="passwordForm.confirmPassword"
             required>
           </v-text-field>
         </v-flex>
-        <v-flex xs6 md4 offset-md8>
-          <v-btn color="green darken-1" @click.native="save">确定</v-btn>
+        <v-flex xs12 md4 offset-md8>
+          <v-btn large color="green darken-1" @click.native="save">确定</v-btn>
         </v-flex>
       </v-layout>
     </v-container>
@@ -39,26 +47,58 @@
 
 
 <script>
-import { httpGet } from '@/utils/api'
+import { httpPut } from '@/utils/api'
+import { mapActions } from 'vuex'
+import bus from '@/utils/bus'
 
 export default {
   name: 'change-password-view',
   data() {
     return {
       passwordForm: {},
+      error: false,
+      errorMessage: '',
     }
   },
-  methods: {
-    loadData() {
-      httpGet('/home/getRecommend').then((response) => {
-        console.log(response)
-      })
-    },
-    save() {
+  computed: {
+    student() {
+      return this.$store.state.account.student
     },
   },
-  created() {
-    this.loadData()
+  methods: {
+    ...mapActions(['STUDENT_LOGOUT']),
+    save() {
+      if (this.passwordForm.password !== this.passwordForm.confirmPassword) {
+        this.error = true
+        this.errorMessage = '两次密码不一致'
+        return
+      }
+      if (!this.passwordForm.password
+            || !this.passwordForm.oldPassword
+            || !this.passwordForm.confirmPassword
+      ) {
+        this.error = true
+        this.errorMessage = '请填写完整的表单'
+        return
+      }
+      const data = this.passwordForm
+      const id = this.student.id
+      httpPut(`/students/password/${id}`, data).then((response) => {
+        if (response.data.status === 201) {
+          bus.$emit('isWarning', {
+            snackbar: true,
+            snackbarTitle: '修改成功，请重新登录',
+            snackbarColor: 'green',
+          })
+          this.teacherPasswordForm = {}
+          this.STUDENT_LOGOUT()
+          this.$router.push('/home')
+        }
+      }).catch((error) => {
+        this.error = true
+        this.errorMessage = error.response.data.message
+      })
+    },
   },
 }
 </script>
